1a7ca125110f80ecec34ac97a1c31de7f0e5da56
Matt Keefer <mrkeefer8@gmail.com>
2025-03-03 18:45:34 -0500

    Logging

--- Users/routes.js
+++ Users/routes.js
@@ -142,7 +142,7 @@ export default function UserRoutes(app) {
       console.log(user);
       if (user) {
         req.session.currentUser = user;
-        console.log('USER COOKIE: ' + req.session);
+        console.log(req.session);
         res.send(user);
       } else {
         res.sendStatus(500);
